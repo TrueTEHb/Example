279a576e242196195ce04a9dea3fa34c3ef76b9f
PavelK <pavielkuz@gmail.com>
2020-04-03 22:14:46 +0300

    change task 3.1.2

--- Spring Task 3.1.2/src/main/java/myApp/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package myApp.repository;
-
-import myApp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User getUserByEmail(String email);
-}
