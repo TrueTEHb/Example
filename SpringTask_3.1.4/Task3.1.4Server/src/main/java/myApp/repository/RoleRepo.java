499c3dc2b99e6e7fa7f963cc2976f8b0fa3f4b16
PavelK <pavielkuz@gmail.com>
2020-04-29 13:08:13 +0300

    edit task 3.1.4

--- SpringTask_3.1.4/Task3.1.4Server/src/main/java/myApp/repository/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package myApp.repository;
-
-import myApp.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    void deleteRolesById(Long id);
-}
